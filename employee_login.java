5637e6e7f41367ccef6ed484118e8a01cedc107f
greeshma-s-y <71713396+greeshma-s-y@users.noreply.github.com>
2022-01-24 12:14:48 +0530

    Create employee_login.java

---
+++ employee_login.java
@@ -0,0 +1,95 @@
+package com.example.employee_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+public class employee_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_login);
+    }
+}*/
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import androidx.annotation.NonNull;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class employee_login  extends AppCompatActivity {
+
+    TextInputEditText etLoginEmail;
+    TextInputEditText etLoginPassword;
+    TextView tvRegisterHere;
+    Button btnLogin,back;
+
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_login);
+
+        etLoginEmail = findViewById(R.id.etRegEmail);
+        etLoginPassword = findViewById(R.id.etLoginPass);
+
+        btnLogin = findViewById(R.id.btnRegister);
+        back=(Button)findViewById(R.id.back);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(view -> {
+            loginUser();
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(getBaseContext(),MainActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+
+    private void loginUser(){
+        String email = etLoginEmail.getText().toString();
+        String password = etLoginPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            etLoginEmail.setError("Email cannot be empty");
+            etLoginEmail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            etLoginPassword.setError("Password cannot be empty");
+            etLoginPassword.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(employee_login.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(employee_login.this, employee_home.class));
+                    }else{
+                        Toast.makeText(employee_login.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+}
